77b8f2e71fc7ebdba2f0710cf22f1ff59a60c838
passieworkflow-dev <passie.workflow@gmail.com>
2026-01-07 13:01:13 +0100

    Update script.js
    sidebar

--- script.js
+++ script.js
@@ -56,10 +56,19 @@ const sidebar = document.getElementById('sidebar')
 function toggleSidebar() {
   sidebar.classList.toggle('close')
   toggleButton.classList.toggle('rotate')
+
+  Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
+    ul.classList.romove('show')
+    ul.previousElementSibling.classList.remove('rotate')
+  })
 }
 function toggleSubMenu(button) {
   button.nextElementSibling.classList.toggle('show')
   button.classList.toggle('rotate')
+
+  if(sidebar.classList.contains('close'))(
+    sidebar.classList.toggle('close')
+    toggleButton.classList.toggle('rotate')
 }
 
 function updateLanguage() {
