e0b49e8c5812adeca2d4071f78c781e651914f81
passieworkflow-dev <passie.workflow@gmail.com>
2026-01-07 14:25:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -57,12 +57,13 @@ function toggleSidebar() {
   sidebar.classList.toggle('close')
   toggleButton.classList.toggle('rotate')
 
-  Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
-    ul.classList.remove('show')
-    ul.previousElementSibling.classList.remove('rotate')
-  })
+  closeAllSubMenus()
 }
+
 function toggleSubMenu(button) {
+
+  closeAllSubMenus()
+  
   button.nextElementSibling.classList.toggle('show')
   button.classList.toggle('rotate')
 
@@ -70,6 +71,13 @@ function toggleSubMenu(button) {
     sidebar.classList.toggle('close')
     toggleButton.classList.toggle('rotate')
   }
+}
+
+function closeAllSubMenus(){
+  Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
+    ul.classList.remove('show')
+    ul.previousElementSibling.classList.remove('rotate')
+  })
 }
 
 function updateLanguage() {
