8d948407261d924e4bf5fd838c142b0f3ce06507
passieworkflow-dev <passie.workflow@gmail.com>
2026-01-07 13:08:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -68,7 +68,6 @@ function toggleSubMenu(button) {
 
   if(sidebar.classList.contains('close'))(
     sidebar.classList.toggle('close')
-    toggleButton.classList.toggle('rotate')
 }
 
 function updateLanguage() {
